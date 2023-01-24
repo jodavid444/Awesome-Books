dd1a820300e2fbf1a3e2363b2a7f115772da45e2
Joseph David <jodavid444dave@gmail.com>
2023-01-24 03:37:42 -0800

    Fix linter errors, ammend styling

--- app.js
+++ app.js
@@ -58,6 +58,9 @@
 
     if (title.value.length < 3 || author.value.length < 1) {
         error.innerHTML = 'input filed must contain characters ';
+        setTimeout(() => {
+            error.innerHTML = '';
+        }, 3000);
     } else {
         error.innerHTML = '';
         libary.add({
@@ -79,6 +82,64 @@
     libary.books.forEach((book) => libary.render(book));
 
 
+
+    const list = [
+        {
+          linkId: 'list',
+          pageId: 'listpage',
+        },
+        {
+          linkId: 'add-new',
+          pageId: 'addpage',
+        },
+        {
+          linkId: 'contact-page',
+          pageId: 'contactpage',
+        },
+      ];
+      
+      list.forEach((item, index) => {
+        const link = document.getElementById(item.linkId);
+        const page = document.getElementById(item.pageId);
+      
+        link.onclick = () => {
+          if (index === 1) {
+            link.style.color = 'rgb(241, 43, 43)';
+            link.previousElementSibling.style.color = '#000';
+            link.nextElementSibling.style.color = '#000';
+            page.classList.remove('hiden');
+            page.previousElementSibling.classList.add('hiden');
+            page.nextElementSibling.classList.add('hiden');
+          } else if (index === 0) {
+            link.style.color = 'rgb(241, 43, 43)';
+            link.nextElementSibling.style.color = '#000';
+            link.nextElementSibling.nextElementSibling.style.color = '#000';
+            page.classList.remove('hiden');
+            page.nextElementSibling.classList.add('hiden');
+            page.nextElementSibling.nextElementSibling.classList.add('hiden');
+          } else {
+            link.style.color = 'rgb(241, 43, 43)';
+            link.previousElementSibling.style.color = '#000';
+            link.previousElementSibling.previousElementSibling.style.color = '#000';
+            page.classList.remove('hiden');
+            page.previousElementSibling.classList.add('hiden');
+            page.previousElementSibling.previousElementSibling.classList.add('hiden');
+          }
+        };
+      });
+      
+      const date = document.querySelector('.date');
+      const d = new Date();
+      const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
+      const mo = new Intl.DateTimeFormat('en', { month: 'long' }).format(d);
+      const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
+      const hour = d.getHours();
+      const minute = d.getMinutes();
+      const second = d.getSeconds();
+      const ampm = hour >= 12 ? 'pm' : 'am';
+      
+      date.innerHTML = `${mo} ${da}th ${ye}, ${hour}:${minute}:${second} ${ampm}`;
+      
 
 
 
